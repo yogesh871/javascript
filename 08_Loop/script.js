c022b7e825621d56f5f1ae6ea7b6d8520fb54c81
yogesh <yogeshrd1708@gmail.com>
2024-12-16 18:38:47 +0530

    loop_excersize 16/12/2024

---
+++ 08_Loop/script.js
@@ -0,0 +1,84 @@
+
+// //  Print 1 to n 
+
+// console.log("1.Print 1 to n")
+// let  n = + prompt("Enter Your Number");
+// for( let i = 1; i <= n; i++){
+//     console.log(i);
+// }
+
+
+//     //  Print n to 1 
+// {
+// console.log("2. Print n to 1 ")
+    
+//     let  i = + prompt("Enter Your Number");
+//     for( let n = 1; i >= n; i--){
+//         console.log(i);
+//     }
+// }
+
+//     //  print -n to n
+// {
+//     // console.log("1. print -n to n")
+
+//     // let a = +prompt("Enter Your Negative  Number ");
+//     // let b = +prompt("Enter Your Positive  Number ");
+
+//     // for(i = a; i <= b; i++){
+//     //     console.log(i);
+//     // }
+// }
+
+
+//  Print odd number
+// {
+    // console.log("4. Print odd number");
+    
+    // let n = +prompt("Enter Your Number");
+    
+    // for(i = 1; i <= n; i++){
+        //      if(i % 2 == 1){
+            //         console.log(i);
+            //      }
+            // }
+// }
+
+
+// print Even Number
+// {
+//     console.log("5. Print even number");
+//     let n = +prompt("Enter Your Number")
+    
+//     for(i = 0; i<= n; i += 2){
+//         console.log(i);
+//     }
+// }
+
+// Print Series 1, 4, 9, 16, 25.....
+// console.log("6. Print Series 1, 4, 9, 16, 25..... ")
+
+// let n = +prompt("Enter Your Number")
+
+// for(let i = 1; i*i <= n; i++){
+//     console.log(i*i);
+// }
+
+
+// Print Series 1, 4, 3, 16, 25.....
+// console.log("7. Print Series 1, 4, 3, 16, 25..... ")
+// let n = +prompt("Enter Your Number")
+
+// for(let i = 1; i <= n; i++){
+
+//     if(i % 2 == 1){
+//         console.log(i);  
+//     }
+//     else{
+//         console.log(i*i)
+//     }
+// }
+
+
+// Print Series 1, 1, 2, 3, 5.....
+console.log("8. print Series 1, 1, 2, 3, 5.....")
