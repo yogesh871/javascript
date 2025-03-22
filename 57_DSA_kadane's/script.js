15295784511487bda7e1ed7c9efb4be104bd65e2
yogesh <yogeshrd1708@gmail.com>
2025-03-22 09:04:07 +0530

    DSA_kanades_Algorithm

---
+++ 57_DSA_kadane's/script.js
@@ -0,0 +1,22 @@
+// kadane's Algorithm
+
+function kadane(arr) {
+    let cursum = 0;
+    let maxsum = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        cursum += arr[i];
+
+        if (cursum > maxsum) {
+            maxsum = cursum;
+        }
+        else if (cursum < 0) {
+            cursum = 0;
+        }
+    }
+    return maxsum;
+    
+}
+
+let arr = [-1, 2, -3, -2, 5, 4, -3];
+console.log(kadane(arr));  
+
