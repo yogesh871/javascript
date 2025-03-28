b985a5fb0e15c393ff47c91cb15925c48e9f2639
yogesh <yogeshrd1708@gmail.com>
2025-03-28 10:23:34 +0530

    DSA_map_&_set

---
+++ 59_DSA_Set_&_Map_Method/script.js
@@ -0,0 +1,12 @@
+//  Set Method  => Given unique value collaction 
+
+let data = new Set( [1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 9])
+
+console.log(data)
+
+console.log(data.add(45))
+
+console.log(data.delete(45))
+
+console.log(data.clear)
+
